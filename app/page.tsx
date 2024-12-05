71b11be93f3db792c7d79a75a117dfd7b59784ea
ignacioLiotti <ignacioliotti@zequenze.com>
2024-12-05 11:52:16 -0300

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from 'lucide-react'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
